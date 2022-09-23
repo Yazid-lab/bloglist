e5e2ea92027aa7f60e995910f6e4fd964e810dbb
Yazid <yazidbougrine99@gmail.com>
2022-09-23 16:47:36 +0100

    helper functions for the test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,31 @@
-const dummy = (blog) => {
+const _ = require('lodash')
+const dummy = () => {
   return 1
 }
-const totalLikes = (blogs) =>{
-    const reducer = (sum, blog)=>{
-        return sum + blog.likes
-    }
-    return blogs.reduce(reducer,0)
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(reducer, 0)
 }
-
-//TODO: exercise 4.5: ADD favoriteBlog function
+const favoriteBlog = (blogs) => {
+  const reducer = (maxLikesBlog, newBlog) => {
+    return newBlog.likes > maxLikesBlog.likes
+      ? {
+          title: newBlog.title,
+          author: newBlog.author,
+          likes: newBlog.likes,
+        }
+      : {
+          title: maxLikesBlog.title,
+          author: maxLikesBlog.author,
+          likes: maxLikesBlog.likes,
+        }
+  }
+  return blogs.reduce(reducer, blogs[0])
+}
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog,
 }
